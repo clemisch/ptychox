012e5c12d80d9dfc3dabdcf540908c61ca945c6d
Clemens Schmid <clemens.schmid@psi.ch>
2024-04-25 16:53:29 +0200

    Switch DM to bilinear shift

--- ptychox/dm.py
+++ ptychox/dm.py
@@ -3,34 +3,37 @@ import jax.numpy as jnp
 
 from . import prop
 
+EPS = 1e-6
+
 
 @jax.jit
 def update_probe(psi, obj, shifts):
-    obj = jax.vmap(jnp.roll, (None, 0, None), 0)(obj, -shifts, (0, 1))
-    psi = jax.vmap(jnp.roll, (0, 0, None), 0)(psi, -shifts, (0, 1))
+    obj = jax.vmap(prop.get_shifted_bilinear, (None, 0))(obj, -shifts)
+    psi = jax.vmap(prop.get_shifted_bilinear, (0, 0))(psi, -shifts)
 
     nom = jnp.sum(jnp.conj(obj) * psi, axis=0)
     denom = jnp.sum(jnp.square(jnp.abs(obj)), axis=0)
-    probe = nom / (denom + 1e-6)
+    probe = nom / (denom + EPS)
 
     return probe
 
 
 @jax.jit
 def update_object(psi, probe, shifts):
-    probes = jax.vmap(jnp.roll, (None, 0, None), 0)(probe, shifts, (0, 1))
+    probes = jax.vmap(prop.get_shifted_bilinear, (None, 0))(probe, shifts)
 
     nom = jnp.sum(jnp.conj(probes) * psi, axis=0)
     denom = jnp.sum(jnp.square(jnp.abs(probes)), axis=0)
-    obj = nom / (denom + 1e-6)
+    obj = nom / (denom + EPS)
 
     return obj
 
 
 @jax.jit
 def set_amplitudes(obj, probe, ampl, shifts):
-    probes = jax.vmap(jnp.roll, (None, 0, None))(probe, shifts, (0, 1))
+    probes = jax.vmap(prop.get_shifted_bilinear, (None, 0))(probe, shifts)
     fwds = jax.vmap(prop.to_farfield)(obj[None] * probes)
-    fwds = ampl * jnp.exp(1j * jnp.angle(fwds))
+    fwds = ampl * fwds / (jnp.abs(fwds) + EPS)
     bcks = jax.vmap(prop.from_farfield)(fwds)
+
     return bcks
