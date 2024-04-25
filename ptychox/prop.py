08f3af0384940db08d78a9ee4985441fe9dac31e
Clemens Schmid <clemens.schmid@psi.ch>
2024-04-25 16:52:29 +0200

    Add lateral shift with bilinear shift to prop.py

--- ptychox/prop.py
+++ ptychox/prop.py
@@ -1,10 +1,36 @@
 import jax
 import jax.numpy as jnp
+import jax.scipy.ndimage as jnd
+
+
+@jax.jit
+def get_shifted_roll(img, shift):
+    out = jnp.roll(img, shift, (0, 1))
+    return out
+
+
+@jax.jit
+def get_shifted_bilinear(img, shift):
+    V, U = img.shape
+    dv, du = shift
+    vv = jnp.arange(V, dtype="float32") - dv
+    uu = jnp.arange(U, dtype="float32") - du
+    coords = jnp.meshgrid(vv, uu, indexing="ij", sparse=True)
+
+    out = jnd.map_coordinates(
+        img, 
+        coords,
+        order=1,
+        mode="constant", 
+        cval=0.0
+    )
+
+    return out
 
 
 @jax.jit
 def to_farfield(field):
-    field = jnp.fft.ifftshift(field)
+    # field = jnp.fft.ifftshift(field)
     fwd = jnp.fft.fft2(field, norm="ortho") 
     fwd = jnp.fft.fftshift(fwd)
 
@@ -15,7 +41,7 @@ def to_farfield(field):
 def from_farfield(field):
     field = jnp.fft.ifftshift(field)
     bck = jnp.fft.ifft2(field, norm="ortho") 
-    bck = jnp.fft.fftshift(bck)
+    # bck = jnp.fft.ifftshift(bck)
 
     return bck
 
@@ -30,7 +56,7 @@ def to_nearfield(field, psize, wlen, dist, M=1.):
     uu = 2. * jnp.pi * jnp.fft.fftfreq(U, psize)
     vv = 2. * jnp.pi * jnp.fft.fftfreq(V, psize)
 
-    # magification (fan to parallel beam)
+    # magification == fan to parallel beam
     # (Fresnel scaling theorem)
     dist = dist / M
 
